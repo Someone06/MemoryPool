eefeb5f4cedf2dbf06fd338b1b84d17f691a882d
Michael Pusl <pusl03@ads.uni-passau.de>
2022-12-13 12:56:31 +0100

    Added: DFS.

--- main.c
+++ main.c
@@ -1,6 +1,65 @@
-#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct Node {
+    void * data;
+    size_t ptrCount;
+    size_t neighbour_count;
+    struct Node* neighbours[];
+};
+
+typedef struct Node Node;
+
+const size_t MARKED_BIT_OFFSET = sizeof(size_t) - 1;
+
+bool is_marked(Node const * const node) {
+    return node->ptrCount >> MARKED_BIT_OFFSET;
+}
+
+void toggle_marked(Node * const node) {
+  node->ptrCount ^= ((size_t) 1) << MARKED_BIT_OFFSET;
+}
+
+Node* new_node(void * const data, const size_t neighbours) {
+   assert(!(neighbours >> MARKED_BIT_OFFSET));
+   Node * const node = calloc(1, sizeof(Node) + sizeof(Node*) * neighbours);
+   node->data = data;
+   node->neighbour_count = neighbours;
+   return node;
+}
+
+void dfs(Node * current, void (* const for_each)(void*)) {
+   Node * previous = NULL, * next = NULL;
+   const bool marking = is_marked(current);
+
+   while(current != NULL) {
+      if(current->ptrCount < current->neighbour_count) {
+          next = current->neighbours[current->ptrCount];
+          if(is_marked(next) != marking) {
+              for_each(next->data);
+              toggle_marked(next);
+              current->neighbours[current->ptrCount] = previous;
+              previous = current;
+              current = next;
+          } else {
+              ++current->ptrCount;
+          }
+      } else {
+          current->ptrCount = 0;
+          next = current;
+          current = previous;
+          if(current != NULL) {
+              previous = current->neighbours[current->ptrCount];
+              current->neighbours[current->ptrCount] = next;
+              ++current->ptrCount;
+          }
+      }
+   }
+}
+
+
 
 int main() {
-    printf("Hello, World!\n");
     return 0;
 }
